3efed7e61937d4243de8194c69b003fe5f84c670
Zubair Afzal <53823564+zubairafzal100@users.noreply.github.com>
2020-06-12 18:36:52 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.<br />
-          Edit By Zubair Afzal
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World! React App made by Zubair Afzal</h1>
     </div>
   );
 }
