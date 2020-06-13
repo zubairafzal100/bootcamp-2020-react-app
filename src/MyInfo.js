6f15e21c7e5abcd1410e2df05e731bc8498f011c
Zubair Afzal <53823564+zubairafzal100@users.noreply.github.com>
2020-06-13 17:53:37 +0500

    Updated MyInfo.js with thanks note

--- src/MyInfo.js
+++ src/MyInfo.js
@@ -5,6 +5,7 @@ function MyInfo (props){
         <div>
             <h1>My name is {props.myName}</h1>
             <h1>My Education is {props.myEdu}</h1>
+            <h1>Thanks Sir Zia Khan and Team for creating this amazing platform for us</h1>
         </div>
     )
 }
